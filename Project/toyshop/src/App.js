7d345cd7736961a71e69070b3fe401da3407905f
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-10-09 02:05:55 +0000

    update header

--- Project/toyshop/src/App.js
+++ Project/toyshop/src/App.js
@@ -1,9 +1,11 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+import Header from "./Header";
+
 function App() {
   return (
-    
+      <Header></Header>
   );
 }
 
--- Project/toyshop/src/Header.js
+++ Project/toyshop/src/Header.js
@@ -1,7 +1,36 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import logo  from './data/img/rubber-duck.png';
+
 function Header(){
 
 
-        
+        return(
+
+            <>
+
+                <nav class="navbar navbar-expand-lg bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand text-white" href="#">
+                        <img src={logo} alt="Logo" width="50" height="50" ></img>
+                        &nbsp;
+                        Nishant Joy Store    
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link text-white" aria-current="page" href="#">Home</a>
+                        
+                    </div>
+                    </div>
+                </div>
+                </nav>
+            
+            </>
+
+
+        );
 
 
 
