31192d7ea824ce38dbfe5c5eea82576ba04477ab
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-07-23 15:34:52 +0530

    update react project

---
+++ Lecture/cwpc01/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+
+
+function App() {
+  return (
+    <h1>welcome</h1>
+  );
+}
+
+export default App;
