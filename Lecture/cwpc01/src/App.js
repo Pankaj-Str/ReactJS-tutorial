f2900d840cdf7a6522822033bd736231af8e82ca
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-07-23 15:49:44 +0530

    update codes....

--- Lecture/cwpc01/src/App.js
+++ Lecture/cwpc01/src/App.js
@@ -2,11 +2,14 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
-
+import Header from './Header.js';
 
 function App() {
   return (
-    <h1>welcome</h1>
+    <>
+      <h1>welcome</h1>
+      <Header />
+    </>
   );
 }
 
---
+++ Lecture/cwpc01/src/Header.js
@@ -0,0 +1,11 @@
+function Header(){
+
+    return(
+        <div>
+            <p>this is header</p>
+        </div>
+    )
+
+}
+
+export default Header;
