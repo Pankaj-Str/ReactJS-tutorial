08876e5344d522a90c65c20fa470f21e56a34283
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-07-25 15:41:41 +0530

    create a header

--- Lecture/cwpc01/src/App.js
+++ Lecture/cwpc01/src/App.js
@@ -1,14 +1,13 @@
 //import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import './css/style.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
 import Header from './Header.js';
-
 function App() {
   return (
     <>
-      <h1>welcome</h1>
-      <Header />
+      <Header></Header>
     </>
   );
 }
--- Lecture/cwpc01/src/Header.js
+++ Lecture/cwpc01/src/Header.js
@@ -1,10 +1,28 @@
+import logo from './img/open-book1.png';
+import Menu from './Menu';
+
 function Header(){
 
-    return(
-        <div>
-            <p>this is header</p>
-        </div>
-    )
+    return (
+        <>
+<nav class="navbar navbar-expand-lg border border-1 border-black">
+  <div class="container">
+    <a class="navbar-brand fs-3 " href="#">
+    <img src={logo} alt="Logo" height="45" width="45" class="image_logo"/>  
+    &nbsp; Joy Book Store
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <Menu></Menu>
+      </ul>
+    </div>
+  </div>
+</nav>
+        </>
+    );
 
 }
 
